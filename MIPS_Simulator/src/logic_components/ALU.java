6ea57302f84611668598b533945ee24451100b5c
Ahmed-wael <ahmedwael2002@hotmail.com>
2015-04-19 15:22:40 +0200

    ALU and registers

---
+++ MIPS_Simulator/src/logic_components/ALU.java
@@ -0,0 +1,15 @@
+package logic_components;
+
+import storage_components.Register;
+
+public class ALU {
+ int zero=0;
+ Register r1;
+ Register r2;
+ int v1;
+ int v2;
+ public ALU(Register r1,Register r2){
+	 
+	 
+ }
+}
